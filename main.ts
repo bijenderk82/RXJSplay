6adf79d14aa83ec31668bd5e6c3be6df874e54da
sbt436 <fnu.bijenderkumar@capitalone.com>
2017-03-28 23:57:40 -0400

    Error Handling using RXJS

--- main.ts
+++ main.ts
@@ -6,23 +6,54 @@ let click = Observable.fromEvent(button, "click")
 
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
+    console.log('welcome');
+    return Observable.create(observer => {
+        console.log('inside observer');
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('load', function() {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(m => {
-            let div = document.createElement('div');
-            div.innerText = m.title;
-            output.appendChild(div);
-        });
-    })
+        xhr.addEventListener('load', () => {
+            if (xhr.statusText == "200") {
+                let data = JSON.parse(xhr.responseText);
+                //renderMovies(data);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+        })
 
-    xhr.open('GET', url);
-    xhr.send();
+        xhr.open('GET', url);
+        xhr.send();
+    }).retryWhen(retryStratgy());
 }
 
-click.subscribe(
-    e => load('movies.json'),
+function retryStratgy() {
+    return function(errors) {
+        return errors
+                .scan((acc, value) => {
+                    console.log(acc, value);
+                    return acc +1;
+                }, 0)
+                .takeWhile(acc => acc < 4)
+                .delay(1000);
+    }
+}
+load('movies.json');
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement('div');
+        div.innerHTML = m.title;
+        output.appendChild(div);
+    })
+}
+
+// click.flatMap(e => load('movies.json'))
+//     .subscribe(o => console.log(o))
+
+click.flatMap(e => load('movies.json'))
+    .subscribe(
+    renderMovies,
     e => console.log(e),
     () => console.log('complete')
-)
+    )
